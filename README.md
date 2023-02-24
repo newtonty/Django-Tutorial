fccca27e59e5d951a2d8e5af7ecc5f83d74688c8
newtonty <nataya.shafira@ui.ac.id>
2023-02-24 19:45:30 +0700

    miawwwww

--- README.md
+++
@@ -1,4 +0,0 @@
-Nama		: Nataya Shafira
-NPM		  : 2106706905
-Kelas		: Z
-Hobi    : Tidur
---
+++ money_tracker/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from money_tracker.models import TransactionRecord
+
+# Create your views here.
+def show_tracker(request):
+    transaction_data = TransactionRecord.objects.all()
+    context = {
+        'list_of_transactions': transaction_data,
+        'name': 'Nataya'
+    }
+    return render(request, "tracker.html", context)
