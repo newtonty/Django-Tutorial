d42af87183cd594fff2872a710832919c666de6f
newtonty <nataya.shafira@ui.ac.id>
2023-02-24 20:11:43 +0700

    miaw miaw

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from money_tracker.views import show_tracker
+from money_tracker.views import create_transaction
+from money_tracker.views import show_xml
+from money_tracker.views import show_json
+from money_tracker.views import show_xml_by_id, show_json_by_id
 
 app_name = 'money_tracker'
 
 urlpatterns = [
     path('', show_tracker, name='show_tracker'),
+    path('create', create_transaction, name='create_transaction'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+
 ]
